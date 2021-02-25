7d57886be9a9e9e3df5f932f087914645ae8b4be
杉山俊介 <sugiyamashunsuke@sugiyamashunsukenoMacBook-puro.local>
2021-02-25 11:18:11 +0900

    config.yml編集

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,8 +1,8 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  access_key_id: 'AKIARHY6H6UNK6RSTWMA',
+  secret_access_key: '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',
   region: "ap-northeast-1",
   bucket: "studio--dig",
 }
